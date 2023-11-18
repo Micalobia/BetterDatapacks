3757410ad08c6e06166e5bb0a7c07377a2a803d9
Micah Neitz <micalobia2@gmail.com>
2023-11-17 20:22:59 -0800

    Initial Commit

---
+++ src/main/java/dev/micalobia/BetterDatapacks.java
@@ -0,0 +1,14 @@
+package dev.micalobia;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterDatapacks implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("better_datapacks");
+
+	@Override
+	public void onInitialize() {
+	}
+}
