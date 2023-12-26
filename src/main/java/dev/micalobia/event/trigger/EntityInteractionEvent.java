4404444a592b806d5182e63b5fe72b7ee94edd13
Micah <micalobia2@gmail.com>
2023-12-26 15:56:45 -0800

    Made Entity interaction event work in spectator

--- src/main/java/dev/micalobia/event/trigger/EntityInteractionEvent.java
+++ src/main/java/dev/micalobia/event/trigger/EntityInteractionEvent.java
@@ -34,8 +34,7 @@ public class EntityInteractionEvent extends Event<
         var result = cancel ? ActionResult.SUCCESS : ActionResult.PASS;
         if (hand == Hand.OFF_HAND) return result;
         if (entityHitResult != null) return result;
-        if (player.isSpectator()) return result;
-        trigger(new Context(server, player, entity));
+        trigger(context);
         return result;
     }
 
